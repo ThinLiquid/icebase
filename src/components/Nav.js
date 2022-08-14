2d4bb17207d3947b50bb396130154f8d794bc226
LiquidDev <mail@liquid.gq>
2022-08-14 02:46:32 +0100

    Better UI

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,12 +1,12 @@
 import React from "react";
 
-import IcebaseBlack from '../assets/images/IcebaseBlack.png';
+import IcebaseWhite from '../assets/images/IcebaseWhite.png';
 
 export default function Nav() {
     return (
         <>
             <nav>
-                <img src={IcebaseBlack} alt="Icebase Music" width="50"></img> <h1>Icebase Music</h1>
+                <img src={IcebaseWhite} alt="Icebase Music" width="50"></img> <h1>Icebase Music</h1>
             </nav>
         </>
     );
